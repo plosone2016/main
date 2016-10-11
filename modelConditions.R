daa764f881fa510e78ca99892ab8d855591c5f4c
smidr <verbalkint@outlook.de>
2016-10-11 22:34:58 +0200

    Update modelConditions.R

--- modelConditions.R
+++ modelConditions.R
@@ -1,4 +1,4 @@
-UIV = function(t,state,parameters) {
+Threestates = function(t,state,parameters) {
   with(as.list(c(state,parameters)),{
     dU = - pBeta*U*V
     dI = pBeta*U*V - pDelta*I
@@ -6,7 +6,14 @@ UIV = function(t,state,parameters) {
     list(c(dU,dI,dV))
   })
 }
-
+UIV <- function(t,state,parameters) {
+  with(as.list(c(state,parameters)),{
+    dU = - pB*U*V
+    dI =   pB*U*V - pD*I
+    dV =   pP*I   - pC*V
+    list(c(dU,dI,dV))
+  })
+}
 times   <- seq(0, 12, by = 0.01)
 state   <- c(U = 10^6, I = 0, V = 10)
 paras   <- c(pBeta = 1e-05, pDelta = 1.6, pP = 5, pC = 3.7)
