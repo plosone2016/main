85e1b31acd1b8168893cf51070cc48a57e370800
plosone2016 <verbalkint@outlook.de>
2016-10-11 22:11:29 +0200

    Update robust.R

--- robust.R
+++ robust.R
@@ -79,3 +79,88 @@ robustODE <- function(LS, init = initPars, crit.Tukey = 0.01, crit.huber = 0.05,
 	       first.w = first.w, first.r = e, 
 	       final.w = current.Tukey, final.r = current.e))
 }
+robustODE <- compiler::cmpfun(robustODE)
+# 1. define RMS function (with weight options)
+LS <- function(x) {
+    pB      <- x[1]
+    pD      <- x[2]
+    pP      <- x[3]
+    pC      <- x[4]
+    paras   <- 10^c(pB = pB, pD = pD, pP = pP, pC = pC)
+    tryCatch( 
+    {
+        out   <- deSolve::ode(state, times, UIV, paras)
+        yhat  <- out[as.character(out[,1]) %in% as.character(cDa$time), "V"]
+        # make sure viral load does not go negative
+        if (any(yhat < 0)) { 
+            message("Predict negative viral load, ABORT!")
+            rms <-  1e+8
+            return(rms)
+        } else {
+            # Number of replicates
+            nm  <- rle(as.character(cDa$time))$lengths
+            # Residuals
+            x   <- cDa$V - rep(log10(yhat), times = nm)
+            if (weighted) rms <- sqrt(mean( w * x^2, na.rm = TRUE))
+            else rms <- sqrt(mean(x^2, na.rm = TRUE))
+            return(rms)
+        }
+    }, error = function(e) {
+            message(e)
+            rms <- 1e+8
+            return(rms)
+        }
+    )
+}
+# function to extract residuals
+resODE <- function(fit) {
+	names(fit$par) <- c("pB", "pD", "pP", "pC")
+	out   <- deSolve::ode(state, times, UIV, 10^(fit$par))
+	yhat  <- out[as.character(out[,1]) %in% as.character(cDa$time), "V"]
+	nm  <- rle(as.character(cDa$time))$lengths
+	x   <- cDa$V - rep(log10(yhat), times = nm)
+	return(x)
+}
+# 1. Options for optim
+initPars <- (lower + upper) / 2
+optim.ctrl <- list(method = "L-BFGS-B", lower = lower, upper = upper, 
+                   control = list(trace=1, factr = 1))  # hessian = TRUE?
+
+# A skew data right
+## Skew normal: Giam phuong sai, thay doi mean
+## ----------------------------------------------------------------------------
+## alpha <- 0
+## mu <- 0
+## omega <- 0.5
+## delta <- alpha / sqrt(1 + alpha^2)
+## mu + omega * delta * sqrt(2 / pi)
+## ----------------------------------------------------------------------------
+exMean <- function(omega = 0.5, alpha =-2) {
+	delta <- alpha / sqrt(1 + alpha^2)
+	return(omega * delta * sqrt(2 / pi))
+}
+## omega^2 * (1 - (2 * delta^2) / pi)
+## ============================================================================
+
+ChooseData2 <- function(timepoints, n.datapoints, std, skew = 0, undetect = FALSE) {
+  ## ------------------------------------------------------
+  ## std is the desire standard deviation
+  ## extract only data in chosen time points
+  ## Using as.character to couple the floating point issue when using %in%
+  ## include undetectable data?
+  ## ------------------------------------------------------
+  sel <- Data0[which(as.character(Data0[, "time"]) %in% as.character(timepoints)), "V"]
+  tmp <- NULL
+  for (i in 1:length(sel)) {
+    # tmp[[i]] <- log10(sel[i]) + rnorm(1000, 0, std) # Normal on log scale
+    tmp[[i]] <- log10(sel[i]) + (sn::rsn(n=1000, xi=0, omega=std, alpha = skew) - exMean(std, skew))
+    tmp[[i]] <- sample(tmp[[i]], n.datapoints, replace = TRUE)
+  }
+  # export from list to data frame and give column names
+  tmp <- cbind(rep(timepoints, each = n.datapoints), unlist(tmp))
+  tmp <- data.frame(tmp)
+  colnames(tmp) <- c("time","V")
+  # remove Undetectable
+  if (!undetect) tmp <- tmp[tmp[, "V"] >= log10(50), ]
+  return(logV = tmp)
+}
